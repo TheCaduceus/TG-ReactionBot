9939b65d0f080f80f4af7127ea924328af491531
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-01-04 19:59:01 +0530

    Add exception handling.

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -1,12 +1,17 @@
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid
 from random import choice
-
-from bot import TelegramBot, logger
+from bot import TelegramBot
 from bot.config import Telegram
 
 @TelegramBot.on_message(filters.all)
 async def send_reaction(_, msg: Message):
-    await msg.react(choice(Telegram.EMOJIS))
-
-logger.info("Bot is now started.")
+    try:
+        await msg.react(choice(Telegram.EMOJIS))
+    except (
+        MessageIdInvalid,
+        EmoticonInvalid,
+        ChatAdminRequired
+    ):
+        pass
