9772172e1f12a7a716ccd5331f70ac498a42dc87
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-01-04 19:59:56 +0530

    Add `BOT_USERNAME`

--- bot/config.py
+++ bot/config.py
@@ -4,6 +4,7 @@ class Telegram:
     API_ID = int(env.get("TG_API_ID", 1234))
     API_HASH = env.get("TG_API_HASH", "xyz")
     BOT_TOKEN = env.get("TG_BOT_TOKEN", "abc")
+    BOT_USERNAME = env.get("TG_BOT_USERNAME", "DrReactBot")
     EMOJIS = [
         "👍", "👎", "❤", "🔥", 
         "🥰", "👏", "😁", "🤔",
